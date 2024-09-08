1b4416334f6d5ff9e3921beb71318ed56ee24a7b
FkyFrky <franck.pagny2@gmail.com>
2024-09-08 16:30:33 +0200

    WIP: Parsing model files

--- source/main.py
+++ source/main.py
@@ -1,1 +1,21 @@
-print('Hello World')
+
+def generate_data():
+    return True
+
+def addLineToModel(line,model):
+    currentObject = None
+    line.split('.')
+
+    return model
+
+def parse_file(path):
+    with open(path,'r') as fileContent:
+        for line in fileContent:
+            model = addLineToModel(line,model)
+    for line in rawContent
+    return True
+
+if __name__ == 'main':
+    parse_file('./examples/models/Airlines.fraz')
+    print('Parsing Fraz file')
+
